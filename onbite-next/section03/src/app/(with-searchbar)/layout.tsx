f9ed32bd39d275cc3d60f0c7ca135065f5494a05
bangsinchur <sinchurbang123@gmail.com>
2025-12-12 19:04:15 +0900

    풀 라우트 캐시,클라이언트 라우터 캐시 설정

--- onbite-next/section03/src/app/(with-searchbar)/layout.tsx
+++ onbite-next/section03/src/app/(with-searchbar)/layout.tsx
@@ -1,15 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import Searchbar from "../../components/searchbar";
 
-export default function Layout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
-      {children}
+      <Suspense fallback={<div>Loding...</div>}>
+        <Searchbar />
+      </Suspense>
+       {children}
     </div>
   );
 }
